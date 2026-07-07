1cb0fd3eb6f91e513e9a86831e692fb2bf137ff9
Daniel Etim <jayleantashar@gmail.com>
2026-07-07 21:04:58 +0100

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -13,7 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles','task',
 ]
 
 MIDDLEWARE = [
