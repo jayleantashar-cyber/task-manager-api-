d3a2ede80b4527be1cf4ceaf01cb3d383f9817b7
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 12:52:10 +0100

    Update urls.py

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -4,3 +4,14 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.http import HttpResponse
+from django.urls import path
+
+def home(request):
+    return HttpResponse("API is Live! Go to /admin/")
+
+urlpatterns = [
+    path('', home),  # add this
+    path('admin/', admin.site.urls),
+    # ... your other urls
+]
