4a216b2dedfe4f158ff1bba04b1f5af938087319
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 15:43:33 +0100

    Update models.py

--- task/models.py
+++ task/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
