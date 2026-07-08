239febb7834a95255ac9d57df63121e1c8e98dfb
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 12:45:00 +0100

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
 DEBUG = False
-ALLOWED_HOSTS = ['task-manager-api-5.onrender.com', 'localhost', '127.0.0.1]
+ALLOWED_HOSTS = ['task-manager-api-5.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
