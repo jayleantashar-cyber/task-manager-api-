d99a770a44d9f1541c69d38baae3fc02bd16685d
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 17:36:05 +0100

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -14,11 +14,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
- 'rest_framework',
-'rest_framework.authtoken',
-'task.apps.TaskConfig',
     'rest_framework',
+    'rest_framework.authtoken',
     'task.apps.TaskConfig',
+    'users.apps.UsersConfig',  # add this line
 ]
 
 
@@ -31,13 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'taskmanager.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+]jangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
