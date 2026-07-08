bbc5c5a16abcf63c405734e5473013b05531b0a6
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 17:25:01 +0100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,1 +1,33 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
 
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class LoginView(APIView):
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key
+            })
+
+        return Response(
+            {"error": "Invalid credentials"},
+            status=400
+        )
