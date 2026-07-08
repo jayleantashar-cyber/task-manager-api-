d0fd4e4e017b700cc23b7f60783b2b348df78c5f
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 10:34:45 +0100

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -57,3 +57,13 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
