4bf138366a75228d0adda6f30af44c5394ab1803
Daniel Etim <jayleantashar@gmail.com>
2026-07-08 15:53:05 +0100

    Update admin.py

--- task/admin.py
+++ task/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import Task
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ['title', 'completed', 'created_at', 'user']
-    list_filter = ['completed']
-    search_fields = ['title']
+admin.site.register(Task)
